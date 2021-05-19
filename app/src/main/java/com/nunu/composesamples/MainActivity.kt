99681f869c83b87b6e9d447de63c0d0e9116e1f0
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-05-19 18:00:04 +0900

    [FEAT] Make Contents text and images

--- app/src/main/java/com/nunu/composesamples/MainActivity.kt
+++ app/src/main/java/com/nunu/composesamples/MainActivity.kt
@@ -3,9 +3,19 @@ package com.nunu.composesamples
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +28,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text (text = "Hello $name!")
-    Text (text = "Hello $name!")
-    Text (text = "Hello $name!")
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(8.dp)),
+            contentScale = ContentScale.FillBounds
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Hello $name!", style = MaterialTheme.typography.h4)
+        Text(
+            text = "A day wandering through the sandhills " +
+                    "in Shark Fin Cove, and a few of the " +
+                    "sights I saw",
+            style = MaterialTheme.typography.body2,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = "Hello $name!",
+            color = MaterialTheme.colors.error,
+            style = MaterialTheme.typography.body1
+        )
+    }
 }
 
 @Preview
