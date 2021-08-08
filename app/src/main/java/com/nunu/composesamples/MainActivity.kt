2cf44dc318850e5207d2dca18336fae6b232b936
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-08-09 01:27:15 +0900

    Theme, Color, Style

--- app/src/main/java/com/nunu/composesamples/MainActivity.kt
+++ app/src/main/java/com/nunu/composesamples/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.nunu.composesamples
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,6 +18,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.nunu.composesamples.theme.ComposeSampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,23 +42,43 @@ fun TextCard(
 fun MessageCard(
     messsage: Message
 ) {
-    Row(modifier = Modifier.padding(8.dp)) {
-        Image(
-            painter = painterResource(R.drawable.ic_launcher_foreground),
-            contentDescription = "Android",
-            modifier = Modifier.size(40.dp).clip(CircleShape)
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Column {
-            Text(text = messsage.author)
-            Spacer(Modifier.height(8.dp))
-            Text(text = messsage.body)
+    ComposeSampleTheme {
+        Row(modifier = Modifier.padding(8.dp)) {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = "Android",
+                modifier = Modifier.size(40.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, MaterialTheme.colors.secondary, CircleShape)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(
+                    text = messsage.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle1
+                )
+                Spacer(Modifier.height(8.dp))
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = 1.dp
+                ) {
+                    Text(
+                        text = messsage.body,
+                        modifier = Modifier.padding(4.dp),
+                        style = MaterialTheme.typography.subtitle2
+                    )
+                }
+            }
         }
     }
-
 }
 
-@Preview
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "DarkMode"
+)
 @Composable
 fun PreviewMessageCard() {
     MessageCard(
